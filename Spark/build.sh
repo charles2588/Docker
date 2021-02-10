3b12fae975d5177072e1e24bc89c40e10d9d9cab
charles <charlesgomes@us.ibm.com>
2021-02-10 01:41:32 -0800

    Fixed image names

--- Spark/build.sh
+++ Spark/build.sh
@@ -1,4 +1,4 @@
-docker build -t sparkbase .
+docker build -t spark3base .
 cd sparkmaster
-docker build -t sparkmaster .
+docker build -t spark3_master .
 docker run -it -d --name spark-master -p 8080:8080 -p 7077:7077 -p 6066:6066 spark3_master:latest  
